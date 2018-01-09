abefba0dac477ab4cea22e5d535548b39c941646
le32zar <le32zar@ppc813.mirz.uni-jena.de>
2018-01-09 13:59:35 +0100

    small changes

--- src/ChatServer/Dialogs/AddRoomDialog.java
+++ src/ChatServer/Dialogs/AddRoomDialog.java
@@ -143,6 +143,9 @@ public class AddRoomDialog extends javax.swing.JDialog {
             java.util.logging.Logger.getLogger(AddRoomDialog.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
 
         /* Create and display the dialog */
         java.awt.EventQueue.invokeLater(new Runnable() {
--- src/ChatServer/Dialogs/RenameRoomDialog.java
+++ src/ChatServer/Dialogs/RenameRoomDialog.java
@@ -7,16 +7,24 @@ package ChatServer.Dialogs;
 
 /**
  *
- * @author le32zar
+ * @author Sleepersword
  */
 public class RenameRoomDialog extends javax.swing.JDialog {
 
+    public boolean Canceled;
+    public String OldRoomName;
+    public String NewRoomName;
+
     /**
-     * Creates new form RenameRoomDialog
+     * Creates new form AddRoomDialog
      */
-    public RenameRoomDialog(java.awt.Frame parent, boolean modal) {
+    public RenameRoomDialog(java.awt.Frame parent, boolean modal, String oldRoomName) {
         super(parent, modal);
         initComponents();
+        
+        Canceled = false;
+        OldRoomName = oldRoomName;
+        labelOldName.setText(oldRoomName);
     }
 
     /**
@@ -28,21 +36,108 @@ public class RenameRoomDialog extends javax.swing.JDialog {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jLabel1 = new javax.swing.JLabel();
+        buttonCancel = new javax.swing.JButton();
+        buttonRename = new javax.swing.JButton();
+        jLabel2 = new javax.swing.JLabel();
+        labelOldName = new javax.swing.JLabel();
+        textFieldNewName = new javax.swing.JTextField();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        setTitle("Add Room...");
+        setAlwaysOnTop(true);
+        addWindowListener(new java.awt.event.WindowAdapter() {
+            public void windowClosing(java.awt.event.WindowEvent evt) {
+                formWindowClosing(evt);
+            }
+        });
+
+        jLabel1.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
+        jLabel1.setText("Old name: ");
+
+        buttonCancel.setText("Cancel");
+        buttonCancel.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                buttonCancelActionPerformed(evt);
+            }
+        });
+
+        buttonRename.setText("Rename Room");
+        buttonRename.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                buttonRenameActionPerformed(evt);
+            }
+        });
+
+        jLabel2.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
+        jLabel2.setText("New name: ");
+
+        labelOldName.setFont(new java.awt.Font("Dialog", 0, 12)); // NOI18N
+        labelOldName.setText("jLabel3");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 393, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel2)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(textFieldNewName))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jLabel1)
+                                .addGap(24, 24, 24)
+                                .addComponent(labelOldName))
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(buttonCancel)
+                                .addGap(212, 212, 212)
+                                .addComponent(buttonRename)))
+                        .addGap(0, 0, Short.MAX_VALUE)))
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 107, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel1)
+                    .addComponent(labelOldName))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(textFieldNewName, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel2))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(buttonCancel)
+                    .addComponent(buttonRename))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void buttonCancelActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buttonCancelActionPerformed
+        Canceled = true;
+        this.setVisible(false);
+    }//GEN-LAST:event_buttonCancelActionPerformed
+
+    private void buttonRenameActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buttonRenameActionPerformed
+        if(textFieldNewName.getText().equals("")) {
+            textFieldNewName.requestFocus();
+            return;
+        }
+
+        NewRoomName = textFieldName.getText();
+        this.setVisible(false);
+    }//GEN-LAST:event_buttonRenameActionPerformed
+
+    private void formWindowClosing(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowClosing
+        // TODO add your handling code here:
+    }//GEN-LAST:event_formWindowClosing
 
     /**
      * @param args the command line arguments
@@ -61,20 +156,21 @@ public class RenameRoomDialog extends javax.swing.JDialog {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(RenameRoomDialog.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AddRoomDialog.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(RenameRoomDialog.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AddRoomDialog.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(RenameRoomDialog.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AddRoomDialog.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(RenameRoomDialog.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AddRoomDialog.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</edibuttonRename
         //</editor-fold>
 
         /* Create and display the dialog */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                RenameRoomDialog dialog = new RenameRoomDialog(new javax.swing.JFrame(), true);
+                AddRoomDialog dialog = new AddRoomDialog(new javax.swing.JFrame(), true);
                 dialog.addWindowListener(new java.awt.event.WindowAdapter() {
                     @Override
                     public void windowClosing(java.awt.event.WindowEvent e) {
@@ -87,5 +183,11 @@ public class RenameRoomDialog extends javax.swing.JDialog {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton buttonCancel;
+    private javax.swing.JButton buttonRename;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel labelOldName;
+    private javax.swing.JTextField textFieldNewName;
     // End of variables declaration//GEN-END:variables
 }
